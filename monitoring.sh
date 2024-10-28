b1d1d5bb14fbd95434f66f08eb350d4912560add
saususge <epiklife@naver.com>
2024-10-28 09:01:32 -0700

    a

--- monitoring.sh
+++ monitoring.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 wall "
-#Architecture: $(uname -a)
-#CPU physical : $(lscpu | grep "^Socket(s):" | awk '{print $2}')
-#vCPU : $(lscpu | grep "^CPU(s):" | awk '{print $2}')
-#Memory Usage: $(free -m | awk '/Mem:/ {printf "%d/%dMB (%.2f%%)\n", $3, $2, $3/$2 * 100}')
-#Disk Usage : $(df -h --total | awk '/^total/ {printf "%s/%s (%s)", $3, $2, $5}')
-#CPU load: $(vmstat 1 1 | tail -1 | awk '{printf "%.1f", 100 - $15}')
-#Last boot: $(who -b | awk '{print $3, $4}')
-#LVM use: $(lsblk | grep -q "lvm" && echo "yes" || echo "no")
-#Connections TCP : $(ss -t | grep ESTAB | wc -l) ESTABLISHED
-#User log : $(who | wc -l)
-#Network : IP $(ip -4 addr show dev enp0s3 | grep inet | awk '{print $2}' | cut -d'/' -f1) ($(ip link show dev enp0s3 | grep link/ether | awk '{print $2}'))
-#Sudo : $(grep -c 'COMMAND' /var/log/sudo/sudolog) cmd
+	#Architecture: $(uname -a)
+	#CPU physical : $(lscpu | grep "^Socket(s):" | awk '{print $2}')
+	#vCPU : $(lscpu | grep "^CPU(s):" | awk '{print $2}')
+	#Memory Usage : $(free -m | awk '/Mem:/ {printf "%d/%dMB (%.2f%%)\n", $3, $2, $3/$2 * 100}')
+	#Disk Usage : $(df -h --total | awk '/^total/ {printf "%s/%s (%s)", $3, $2, $5}')
+	#CPU load: $(vmstat 1 1 | tail -1 | awk '{printf "%.1f", 100 - $15}')%
+	#Last boot: $(who -b | awk '{print $3, $4}')
+	#LVM use: $(lsblk | grep -q "lvm" && echo "yes" || echo "no")
+	#Connections TCP : $(ss -t | grep ESTAB | wc -l) ESTABLISHED
+	#User log : $(who | wc -l)
+	#Network : IP $(ip -4 addr show dev enp0s3 | grep inet | awk '{print $2}' | cut -d'/' -f1) ($(ip link show dev enp0s3 | grep link/ether | awk '{print $2}'))
+	#Sudo : $(grep -c 'COMMAND' /var/log/sudo/sudolog) cmd
 "
-
